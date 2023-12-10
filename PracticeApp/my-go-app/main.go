770cdf9d83d1dbabb859d7ccc7214e3d5bd3f155
Aqsa Fatima <aqsafatima0202@gmail.com>
2023-12-10 23:49:43 +0500

    Commit

--- PracticeApp/my-go-app/main.go
+++ PracticeApp/my-go-app/main.go
@@ -1,17 +1,42 @@
 package main
 
 import (
-	"log"
+	"database/sql"
+	"fmt"
 	"net/http"
+
+	_ "github.com/denisenkom/go-mssqldb"
 )
 
+var db *sql.DB
+
 func main() {
+	connString := "server=LAPTOP-G5TDHLRV\\SQL_IAD;port=1433;database=learning;user id=Final_Year_Project;password=fyp;"
+	var err error
+	db, err = sql.Open("sqlserver", connString)
+	if err != nil {
+		fmt.Println("Error connecting to the database:", err)
+		return
+	}
+	defer db.Close()
+
+	// Test the connection
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Error pinging database:", err.Error())
+		return
+	}
+
+	fmt.Println("Connected to the database!")
+
 	fs := http.FileServer(http.Dir("../my-vue-app/dist"))
 	http.Handle("/", fs)
 
-	log.Println("Listening on :8080...")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	http.HandleFunc("/api/login", loginAPI)
+
+	// Serve static files (CSS, JavaScript, etc.)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	fmt.Println("Server started on :8080")
+	http.ListenAndServe(":8080", nil)
 }
--- PracticeApp/my-vue-app/src/App.vue
+++ PracticeApp/my-vue-app/src/App.vue
@@ -3,7 +3,20 @@
 </script>
 
 <template>
-  
+  <h1>Login</h1>
+    <form action="/api/login" method="post">
+        <div>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <div>
+            <button type="submit">Login</button>
+        </div>
+    </form>
 </template>
 
 <style scoped>
