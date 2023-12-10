ebefa0c24d542619bc1cea56f046b055dcf8e3ab
Aqsa Fatima <aqsafatima0202@gmail.com>
2023-12-10 23:01:54 +0500

    Commit

--- VueGolang_CombinedAPP/frontend/Calculator/src/main.js
+++ VueGolang_CombinedAPP/frontend/Calculator/src/main.js
@@ -1,6 +1,10 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(BootstrapVue)
+app.mount('#app')
